094c6da43196daa3ec7f58118ac6331c802b1275
Luna Rae <kra7888@gmail.com>
2023-03-25 17:20:56 -0400

    new file names

--- models/index.js
+++ models/index.js
@@ -1,6 +1,6 @@
-const User = require('./User');
-const Post = require('./Post');
-const Comment = require('./Comment');
+const Post = require('./post');
+const Comment = require('./comment');
+const User = require('./user');
 
 Post.belongsTo(User, {
   foreignKey: 'user_id',
